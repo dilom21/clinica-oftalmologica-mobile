8c10b76295aae5fa013c90dba074ec0b5d51706d
harxld069 <hamadorona@gmail.com>
2026-09-08 04:09:42 -0400

    config: consumir backend desplegado en Render

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -6,6 +6,6 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://10.0.2.2:8000',
+    defaultValue: 'https://clinica-oftalmologica-api.onrender.com',
   );
 }
